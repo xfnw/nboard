dee61ad017b656e9d9aabda6dee8571783436475
xfnw <xfnw@ttm.sh>
2020-07-24 17:13:53 +0000

    ctrl+arrow for faster movinh

--- getkey.py
+++ getkey.py
@@ -4,6 +4,13 @@ import curses
 def main(stdscr):
     while True:
         k = stdscr.getkey()
+
+        if k == '\x1b':
+            k=""
+            for i in range(5):
+                k += stdscr.getkey()
+
+
         print(repr(k))
 
 
