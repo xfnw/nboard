2570562a8d98b83b37b90546bcf162b39ecd2d50
xfnw <xfnw@riseup.net>
2022-01-26 21:29:17 -0500

    reformat with black

--- clean.py
+++ clean.py
@@ -2,15 +2,14 @@
 
 import json
 
-with open('nboard.json', 'r') as file:
+with open("nboard.json", "r") as file:
     data = json.load(file)
 
 print(len(data))
 
-data = {k:v for k, v in data.items() if v is not " "}
+data = {k: v for k, v in data.items() if v is not " "}
 
 print(len(data))
 
-with open('nboard.json', 'w') as file:
+with open("nboard.json", "w") as file:
     json.dump(data, file)
-
--- getkey.py
+++ getkey.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 import curses
 
+
 def main(stdscr):
     while True:
         k = stdscr.getkey()
 
-        if k == '\x1b':
-            k=""
+        if k == "\x1b":
+            k = ""
             for i in range(5):
                 k += stdscr.getkey()
-
 
         print(repr(k))
 
 
-
 curses.wrapper(main)
-
-
